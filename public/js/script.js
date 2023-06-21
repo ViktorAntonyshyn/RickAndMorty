ac7bea293e5e38e9d82404b7d317494c072168be
Viktor.A <viktor.antonyshyn@gmail.com>
2023-06-21 05:59:45 +0200

    All small card works

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,31 @@
-const urlApi = 'https://rickandmortyapi.com/api/character/1'
-
-let textAPI1 = document.getElementById('text-API1');
-let imageElement = document.getElementById('pic-1');
+const urlApi = 'https://rickandmortyapi.com/api/character';
 
 const config = {
     headers: {
         Accept: "application/json",
     },
-}
+};
 
 fetch(urlApi, config)
-.then((respuesta) => respuesta.json())
-.then((data) => {
-    textAPI1.innerHTML = `${data.name} <br> ${data.gender} <br> ${data.status}`;
-    imageElement.src = data.image;
-    statusElement.innerHTML = data.status;
-})
+  .then((response) => response.json())
+  .then((data) => {
+    const characters = data.results;
+
+    characters.slice(0, 20).forEach((character, index) => {
+      const characterId = index + 1;
+      const characterName = character.name;
+      const characterImage = character.image;
+      const characterStatus = character.status;
+      const characterGender = character.gender;
+
+      const characterElement = document.getElementById(`text-API${characterId}`);
+      const imageElement = document.querySelector(`#pic${characterId}`);
+
+      characterElement.innerHTML = `${characterName}<br>${characterGender}<br>${characterStatus}`;
+      imageElement.src = characterImage;
+      imageElement.alt = characterName;
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
